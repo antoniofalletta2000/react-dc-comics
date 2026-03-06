391749cd1efefbd1ee6fe35306fa66f606b6eb90
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-06 13:56:30 +0100

    install and import bootstrap and add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import dcLogo from "/img/dc-logo.png"
+
+
 export default function Header(){
     return(
-        <header>
-            Ciao
+        <header className="d-flex justify-content-around">
+            <img className="mt-3" src={dcLogo} alt="" />
+            <nav className="d-flex gap-3 align-items-center ">
+                <a href="">CHARACTERS</a>
+                <a className="active" href="">COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+            </nav>
         </header>
     )
 }
