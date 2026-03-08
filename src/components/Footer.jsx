805eef0b138c2ab70a24260cae3c1687176bca99
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-08 17:48:50 +0100

    add responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,98 +8,113 @@ import periscopeLogo from "/img/footer-periscope.png"
 export default function Footer() {
     return (
         <>
+
             <div id="bgimg" className="bgimg">
-                <div className="container text-white d-flex justify-content-start">
-                    <div className="pt-5 d-flex justify-content-center gap-5">
+                <div className="container-fluid text-white d-flex ">
+                    <div className="row row-cols-1 row-cols-md-2  row-cols-lg-4 pt-5 d-flex justify-content-center gap-5">
 
+                        <div className="col">
+                            <ul>
+                                <li>
+                                    <h3>DC COMICS</h3>
+                                </li>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                                <li className="pt-4">
+                                    <h3>SHOP</h3>
+                                </li>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
 
-                        <ul>
-                            <li>
-                                <h3>DC COMICS</h3>
-                            </li>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                            <li className="pt-4">
-                                <h3>SHOP</h3>
-                            </li>
-                            <li>Shop DC</li>
-                            <li>Shop DC Collectibles</li>
-                        </ul>
+                        <div className="col">
+                            <ul>
+                                <li>
+                                    <h3>DC</h3>
+                                </li>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy (New) </li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshop</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
 
+                        <div className="col">
+                            <ul>
+                                <li>
+                                    <h3>SITES</h3>
+                                </li>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
 
-
-                        <ul>
-                            <li>
-                                <h3>DC</h3>
-                            </li>
-                            <li>Terms Of Use</li>
-                            <li>Privacy policy (New) </li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshop</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                            <li>Shop Help</li>
-                            <li>Contact Us</li>
-                        </ul>
-
-
-                        <ul>
-                            <li>
-                                <h3>SITES</h3>
-                            </li>
-                            <li>DC</li>
-                            <li>MAD Magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>DC Power Visa</li>
-                        </ul>
-
-
+                        <div className="col">
+                            <img id="dcbg" className="img-fluid" src={dcLogoBg} alt="" />
+                        </div>
                     </div>
-                    <img id="dcbg" src={dcLogoBg} alt="" />
                 </div>
             </div>
 
 
 
+            <div id="line_grey">
+                <div className="container pt-3">
+                <div className="row row-cols-1 row-cols-md-2 d-flex justify-content-between justify-content-center align-items-center">
+                    <div className="col">
+                        <div>
+                            <button className="border border-info border-3 bg-transparent p-3 text-white fw-bold">
+                                SIGN-UP NOW!
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col ">
+                        <div className="social_icons d-flex justify-content-end gap-3 align-items-center ">
+                            <span className="text-info fw-bold">FOLLOW US</span>
+                            <a href="">
+                                <img src={facebookLogo} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={twitterLogo} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={youtubeLogo} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={pinterestLogo} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={periscopeLogo} alt="" />
+                            </a>
 
-            <div id="line_grey" className="line_grey d-flex justify-content-around align-items-center">
-                <div>
-                    <button className="border border-info border-3 bg-transparent p-3 text-white fw-bold">
-                        SIGN-UP NOW!
-                    </button>
+                        </div>
+                    </div>
+
+
+
                 </div>
 
-                <div className="social_icons d-flex gap-3 align-items-center">
-                    <span className="text-info fw-bold">FOLLOW US</span>
-                    <a href="">
-                        <img src={facebookLogo} alt="" />
-                    </a>
-                    <a href="">
-                        <img src={twitterLogo} alt="" />
-                    </a>
-                    <a href="">
-                        <img src={youtubeLogo} alt="" />
-                    </a>
-                    <a href="">
-                        <img src={pinterestLogo} alt="" />
-                    </a>
-                    <a href="">
-                        <img src={periscopeLogo} alt="" />
-                    </a>
-
-                </div>
             </div>
+
+            </div>
+            
         </>
-
 
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,30 @@
 import dcLogo from "/img/dc-logo.png"
 
 
-export default function Header(){
-    return(
-        <header className="d-flex justify-content-around">
-            <img className="mt-3" src={dcLogo} alt="" />
-            <nav className="d-flex gap-3 align-items-center ">
-                <a href="">CHARACTERS</a>
-                <a className="active border-bottom border-primary border-5" href="">COMICS</a>
-                <a href="">MOVIES</a>
-                <a href="">TV</a>
-                <a href="">GAMES</a>
-                <a href="">COLLECTIBLES</a>
-                <a href="">VIDEOS</a>
-                <a href="">FANS</a>
-                <a href="">NEWS</a>
-                <a href="">SHOP</a>
-            </nav>
+export default function Header() {
+    return (
+        <header className=" container">
+            <div className="row row-cols-1 d-flex justify-content-between align-items-center">
+                <div className="col col-md-4">
+                    <img className="m-3" src={dcLogo} alt="" />
+                </div>
+                <div className="col col-md-8">
+                    <nav className="d-flex gap-2 align-items-center justify-content-center">
+                        <a href="">CHARACTERS</a>
+                        <a className="active border-bottom border-primary border-5" href="">COMICS</a>
+                        <a href="">MOVIES</a>
+                        <a href="">TV</a>
+                        <a href="">GAMES</a>
+                        <a href="">COLLECTIBLES</a>
+                        <a href="">VIDEOS</a>
+                        <a href="">FANS</a>
+                        <a href="">NEWS</a>
+                        <a href="">SHOP</a>
+                    </nav>
+                </div>
+
+            </div>
+
         </header>
     )
 }
